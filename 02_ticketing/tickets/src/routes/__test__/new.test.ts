2e2d3ef7207f6714e3a89e9a6cbd02be3e4a1b83
yong liu <am.liuyong@qq.com>
2021-01-02 17:05:40 +0800

    new

--- 02_ticketing/tickets/src/routes/__test__/new.test.ts
+++ 02_ticketing/tickets/src/routes/__test__/new.test.ts
@@ -1,8 +1,7 @@
 import request from 'supertest';
 import {app} from '../../app';
 import {Ticket} from '../../models/ticket';
-
-jest.mock('../../nats-wrapper');
+import {natsWrapper} from '../../nats-wrapper';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
     const response = await request(app).post('/api/tickets').send({});
@@ -81,3 +80,20 @@ it('creates a ticket with valid inputs', async () => {
     expect(tickets[0].price).toEqual(20);
     expect(tickets[0].title).toEqual(title);
 });
+
+it ('publishes an event', async () => {
+    const title = 'asldkfj';
+
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title,
+            price: 20,
+        })
+        .expect(201);
+   // console.log(natsWrapper);
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+});
+
