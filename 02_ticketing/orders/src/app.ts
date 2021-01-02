806ef9764254b8420557d6fd12ed3519d0fddf41
yong liu <am.liuyong@qq.com>
2021-01-02 19:17:44 +0800

    new

--- 02_ticketing/orders/src/app.ts
+++ 02_ticketing/orders/src/app.ts
@@ -10,6 +10,7 @@ import { newOrderRouter } from './routes/new';
 import { showOrderRouter } from './routes/show';
 
 const app = express();
+
 app.set('trust proxy', true);
 app.use(json());
 app.use(
