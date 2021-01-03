6c5a58711bcfd4c68fbe19f5a5d91a78f7f5de5f
yong liu <am.liuyong@qq.com>
2021-01-03 13:23:21 +0800

    new

--- 02_ticketing/orders/src/routes/delete.ts
+++ 02_ticketing/orders/src/routes/delete.ts
@@ -1,34 +1,37 @@
-import express, { Request, Response } from 'express';
-import {
-  requireAuth,
-  NotFoundError,
-  NotAuthorizedError,
-} from '@yltickets/common';
-import { Order, OrderStatus } from '../models/order';
+import express, {Request, Response} from 'express';
+import {NotAuthorizedError, NotFoundError, requireAuth,} from '@yltickets/common';
+import {Order, OrderStatus} from '../models/order';
+import {OrderCancelledPublisher} from "../events/publishers/order-cancelled-publisher";
+import {natsWrapper} from "../nats-wrapper";
 
 const router = express.Router();
 
 router.delete(
-  '/api/orders/:orderId',
-  requireAuth,
-  async (req: Request, res: Response) => {
-    const { orderId } = req.params;
+    '/api/orders/:orderId',
+    requireAuth,
+    async (req: Request, res: Response) => {
+        const {orderId} = req.params;
 
-    const order = await Order.findById(orderId);
+        const order = await Order.findById(orderId).populate('ticket');
 
-    if (!order) {
-      throw new NotFoundError();
+        if (!order) {
+            throw new NotFoundError();
+        }
+        if (order.userId !== req.currentUser!.id) {
+            throw new NotAuthorizedError();
+        }
+        order.status = OrderStatus.Cancelled;
+        await order.save();
+
+        // publishing an event saying this was cancelled!
+        new OrderCancelledPublisher(natsWrapper.client).publish({
+            id: order.id,
+            ticket: {
+                id: order.ticket.id
+            }
+        });
+        res.status(204).send(order);
     }
-    if (order.userId !== req.currentUser!.id) {
-      throw new NotAuthorizedError();
-    }
-    order.status = OrderStatus.Cancelled;
-    await order.save();
-
-    // publishing an event saying this was cancelled!
-
-    res.status(204).send(order);
-  }
 );
 
-export { router as deleteOrderRouter };
+export {router as deleteOrderRouter};
